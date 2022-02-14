2bd8e88910d268253246a83c742cf19a5e56807d
james <90643139+Jamcha123@users.noreply.github.com>
2022-02-14 10:34:14 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { PointsMaterial } from 'three';
 import './style.css';
 
 const scene = new THREE.Scene();
@@ -9,71 +8,50 @@ camera.rotation.z = 1;
 camera.rotation.x = Math.PI / 2;
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+    canvas: document.querySelector('#bg'),
 });
-renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-camera.position.set(5, 5, 5);
+camera.position.setZ(30);
 renderer.render(scene, camera);
 
 var x = document.querySelector('header');
 document.body.appendChild(x);
 
-const lights = new THREE.DirectionalLight(0xffffff);
-lights.position.set(0, 1, 0);
-lights.castShadow = true;
-
+const lights = new THREE.DirectionalLight({color: 0xFFFFFF32});
+lights.position.set(5, 5, 5);
 lights.shadow.mapSize.width = 512;
 lights.shadow.mapSize.height = 512;
 lights.shadow.camera.near = 0.1;
-lights.shadow.camera.far = 100;
+lights.shadow.camera.far = 1000;
+
+scene.add(lights);
 
 const vertices = [];
-
 for(let i = 0; i < 10000; i++){
-  const x = THREE.MathUtils.randFloatSpread(2000);
-  const y = THREE.MathUtils.randFloatSpread(2000);
-  const z = THREE.MathUtils.randFloatSpread(2000);
+    const x = THREE.MathUtils.randFloatSpread(2000);
+    const y = THREE.MathUtils.randFloatSpread(2000);
+    const z = THREE.MathUtils.randFloatSpread(2000);
 
-  vertices.push(x, y, z);
+    vertices.push(x, y, z);
 }
-const geomtry = new THREE.BufferGeometry();
-geomtry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+const stargeomtry = new THREE.BufferGeometry();
+stargeomtry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
 
-const material = new PointsMaterial({color: 0xFFFFFF32});
+const starmaterial = new THREE.PointsMaterial({color: 0xaaaaaa, size: 1.5});
+const stars = new THREE.Points(stargeomtry, starmaterial);
+stars.castShadow = true;
+stars.receiveShadow = false;
 
-const point = new THREE.Points(geomtry, material);
-point.castShadow = true;
-point.receiveShadow = false;
-scene.add(point);
+scene.add(stars);
 
 renderer.render(scene, camera);
-
 window.addEventListener('mousemove', (e) => {
-  point.position.x = Math.random() * 600 - 300;
-  point.position.y = Math.random() * 600 - 300;
-  point.position.z = Math.random() * 600 - 300;
+    stars.rotation.x += 0.01;
+    stars.rotation.y += 0.02;
+    stars.rotation.z += 0.01;
 
-  renderer.render(scene, camera);
-});
-const spheregeomtry = new THREE.SphereGeometry(200, 300, 400);
-const loader = new THREE.TextureLoader().load('https://tse3.mm.bing.net/th?id=OIP.LhXpzQ_6j29qGmfVUIawPAHaHZ&pid=Api');
-const spherematerial = new THREE.MeshBasicMaterial({map: loader});
-const sphere = new THREE.Mesh(spheregeomtry, spherematerial);
-sphere.castShadow = true;
-sphere.receiveShadow = false;
-scene.add(sphere);
-
-function animate(){
-  requestAnimationFrame(animate);
-
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.02;
-  sphere.rotation.z += 0.01;
-
-  renderer.render(scene, camera);
-}
-animate();
-
+    renderer.render(scene, camera);
+})
