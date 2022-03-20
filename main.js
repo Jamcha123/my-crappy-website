ae4c93c1d69c4e49d2165d8e2dad18e6970f35ae
james <90643139+Jamcha123@users.noreply.github.com>
2022-03-20 21:09:46 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,56 +1,51 @@
 import * as THREE from 'three';
 import './style.css';
 
-const scene = new THREE.Scene(); 
+const scene1 = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 500);
+const scene2 = new THREE.Scene();
+scene2.background = new THREE.Color(0xcccccc);
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),
+const camera1 = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / -2, window.innerHeight / 2, 1, 1000);
+camera1.rotation.z = 1;
+camera1.rotation.x = Math.PI / 2;
+
+const camera2 = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 500);
+camera2.position.set(0, 0, 100);
+camera2.lookAt(0, 0, 0);
+
+const renderer1 = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg1'),
 });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.outputEncoding = THREE.sRGBEncoding;
-camera.position.set(0, 0, 100);
-camera.lookAt(0,0,0);
-renderer.render(scene, camera);
+renderer1.setPixelRatio(window.devicePixelRatio);
+renderer1.setSize(window.innerWidth, window.innerHeight);
+renderer1.shadowMap.enabled = true;
+renderer1.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer1.outputEncoding = THREE.sRGBEncoding;
+renderer1.render(scene1, camera1);
+const renderer2 = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg2'),
+});
+renderer2.setPixelRatio(window.devicePixelRatio);
+renderer2.setSize(window.innerWidth, window.innerHeight);
+renderer2.shadowMap.enabled = true;
+renderer2.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer2.outputEncoding = THREE.sRGBEncoding;
+renderer2.render(scene2, camera2);
 
-const material = new THREE.LineBasicMaterial({color:0x2214ff});
+const vertices = [];
+for(let i = 0; i < 10000; i++){
+    const x = THREE.MathUtils.randFloatSpread(2000);
+    const y = THREE.MathUtils.randFloatSpread(2000);
+    const z = THREE.MathUtils.randFloatSpread(2000);
 
-const points = [];
-for(let i = 0; i < 1000; i++){
-    points.push(new THREE.Vector3(
-        Math.random() * window.innerWidth - window.innerHeight,
-        Math.random() * window.innerWidth - window.innerHeight,
-        Math.random() * window.innerWidth - window.innerHeight
-    ));
-    points.push(new THREE.Vector3(
-        Math.random() * window.innerWidth - window.innerHeight,
-        Math.random() * window.innerWidth - window.innerHeight,
-        Math.random() * window.innerWidth - window.innerHeight
-    ));
-    points.push(new THREE.Vector3(
-        Math.random() * window.innerWidth - window.innerHeight,
-        Math.random() * window.innerWidth - window.innerHeight,
-        Math.random() * window.innerWidth - window.innerHeight
-    ));
+    vertices.push(x, y, z);
 }
+const stargeomtry = new THREE.BufferGeometry();
+stargeomtry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
 
-const geomtry = new THREE.BufferGeometry().setFromPoints(points);
+const starmaterial = new THREE.PointsMaterial({color:0xff3221, size: 3});
+const stars = new THREE.Points(stargeomtry, starmaterial);
+scene1.add(stars);
 
-const lines = new THREE.Line(geomtry, material);
-
-scene.add(lines);
-
-function animate(){
-    requestAnimationFrame(animate);
-
-    lines.rotation.x += 0.01;
-    lines.rotation.y += 0.01;
-    lines.rotation.z += 0.01;
-
-    renderer.render(scene, camera);
-}
-animate();
+renderer1.render(scene1, camera1);
