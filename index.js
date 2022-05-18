315757fc6b5cccc7aabe8ff06c9ddad91b8050b0
james <90643139+Jamcha123@users.noreply.github.com>
2022-05-18 21:34:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ const firebaseApp = initializeApp({
     storageBucket: "let-sg.appspot.com",
     messagingSenderId: "157930628762",
     appId: "1:157930628762:web:a5d37e53f742f03befe123",
-    measurementId: "G-ZPN24SWGL0"  
+    measurementId: "G-ZPN24SWGL0"
+  
 });
 
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
 
 onAuthStateChanged(auth, user => {
-    if(user == null){
+    if(user => null){
         console.log("user logged in");
     }else{
         console.log("user not found");
--- main.js
+++ main.js
@@ -12,64 +12,55 @@ camera.position.setZ(30);
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg1'),
 });
-renderer.setPixelRatio(window.setPixelRatio);
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.physicallyCorrectLights = true;
-renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.outputEncoding = THREE.sRGBEncoding; 
 renderer.render(scene, camera);
 
 const controls = new orbit(camera, renderer.domElement);
 
-const vertices = [];
-for(let i = 0; i < 200; i++){
-    const x = new THREE.Vector3(
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300
-    );
-    const y = new THREE.Vector3(
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300
-    );
-    const z = new THREE.Vector3(
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300
-    );
-    vertices.push(x, y, z);
-}
-const linegeomtry = new THREE.BufferGeometry().setFromPoints(vertices);
-
-const linematerial = new THREE.LineBasicMaterial({color: 0x0000ff});
-const lines = new THREE.Line(linegeomtry, linematerial);
-scene.add(lines);
-
-var linevelocity = 0;
-var lineaccelaration = 0.2;
-
-var posx = 0;
-var posy = 0;
-var num = 0;
-function animate(){
-    requestAnimationFrame(animate);
-    num++;
-
-    linevelocity += lineaccelaration;
-    lines.position.z = linevelocity;
-    posx += 0.005;
-    lines.rotation.x = posx;
-    posy += 0.005;
-    lines.rotation.y = posy;
-
-    if(num >= 200){
-        num = 0;
-        posx = 0;
-        posy = 0;
-        linevelocity = 0;
+function init(){
+    let y = 20000;
+    const vertices = [];
+    for(let i = 0; i < y; i++){
+        const x = THREE.MathUtils.randFloatSpread(2000);
+        const y = THREE.MathUtils.randFloatSpread(2000);
+        const z = THREE.MathUtils.randFloatSpread(2000);
+        
+        vertices.push(x, y, z);
     }
-    renderer.render(scene, camera);
+    const stargeomtry = new THREE.BufferGeometry();
+    stargeomtry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    
+    const starmaterial = new THREE.PointsMaterial({color: 0xaaaaaa, size: 1.5});
+    const stars = new THREE.Points(stargeomtry, starmaterial);  
+    scene.add(stars);
+
+    var starvelocity = 0;
+    var staracceraltion = 0.5;
+    
+    var num = 0;
+
+    const lights = new THREE.AmbientLight(0xaaaaaa);
+    scene.add(lights);
+
+    function animate(){
+        requestAnimationFrame(animate);
+        num++;
+    
+        starvelocity += staracceraltion;
+        stars.position.z = starvelocity;
+    
+        if(num >= 500){
+            num = 0;
+            starvelocity = 0;
+        }
+    
+        renderer.render(scene, camera);
+    }
+    animate();
 }
-animate();
+init();
